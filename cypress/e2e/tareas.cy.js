eba66f9ed6a64d1c803bd02abe66cc5997c1b22b
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-11-07 21:09:05 -0400

    implementacion prueba cypress: deberia cambiar a la pagina de estudiantes al hacer clicl en el boton estudainte

--- cypress/e2e/tareas.cy.js
+++ cypress/e2e/tareas.cy.js
@@ -1,6 +1,10 @@
 describe("cambiar paginas", () => {
-    it("deberia cambiar a la pagina de docentes al hacer clicl en el boton", () => {
+    it("deberia cambiar a la pagina de docentes al hacer clicl en el boton docente", () => {
     cy.visit('http://127.0.0.1:5500/docente.html');
     cy.get("#pagDocente").should("contain", "Docente");
+    });
+    it("deberia cambiar a la pagina de estudiantes al hacer clicl en el boton estudainte", () => {
+    cy.visit('http://127.0.0.1:5500/estudiante.html');
+    cy.get("#pagEstudiante").should("contain", "Estudiante");
     });
 });
