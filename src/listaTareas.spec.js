a42af10e6119d020bc1c1d48e9a5199e7e034709
younglee <young.lee@ucb.edu.bo>
2022-11-10 09:31:55 -0400

    cypress y pruebas unit corregidas

--- src/listaTareas.spec.js
+++ src/listaTareas.spec.js
@@ -17,9 +17,9 @@ describe("manejador tarea", () => {
     it("deberia agregar mas de una tarea", () => {
         const resultado1 = new Tarea("Bda2","Mongo db","llenar base de datos","12-04-2022");
         tareas.agregarTarea(resultado1)
-        const resultado2 = new Tarea("Bda1","Mongo db","llenar base de datos","2022-12-04");
+        const resultado2 = new Tarea("Bda1","Mongo db","llenar base de datos","2022-12-05");
         tareas.agregarTarea(resultado2)
-        expect(tareas.mostrarlista()).toEqual("<br> Fecha:2022-12-04<br> Materia: Bda2 Tarea: Mongo db<br> Fecha:2022-12-04<br> Materia: Bda1 Tarea: Mongo db");
+        expect(tareas.mostrarlista()).toEqual("<br> Fecha:2022-12-04<br> Materia: Bda2 Tarea: Mongo db<br> Fecha:2022-12-05<br> Materia: Bda1 Tarea: Mongo db");
         
       });
 
