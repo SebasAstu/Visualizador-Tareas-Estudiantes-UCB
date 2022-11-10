65ab253580b5f3264b21755923b056d39f1392d5
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-11-09 21:01:11 -0400

    implemetar unit test: deberia agregar mas de dos tareas

--- src/listaTareas.spec.js
+++ src/listaTareas.spec.js
@@ -22,5 +22,16 @@ describe("manejador tarea", () => {
         expect(tareas.mostrarlista()).toEqual("<br> Fecha:12-04-2022<br> Materia: Bda2 Tarea: Mongo db<br> Fecha:12-04-2022<br> Materia: Bda1 Tarea: Mongo db");
         
       });
+
+      it("deberia agregar mas de dos tareas", () => {
+        const resultado1 = new Tarea("Bda2","Mongo db","llenar base de datos","12-04-2022");
+        tareas.agregarTarea(resultado1)
+        const resultado2 = new Tarea("Bda1","Mongo db","llenar base de datos","12-04-2022");
+        tareas.agregarTarea(resultado2)
+        const resultado3 = new Tarea("tallerbda","Mongo db","llenar base de datos","12-04-2022");
+        tareas.agregarTarea(resultado3)
+        expect(tareas.mostrarlista()).toEqual("<br> Fecha:12-04-2022<br> Materia: Bda2 Tarea: Mongo db<br> Fecha:12-04-2022<br> Materia: Bda1 Tarea: Mongo db<br> Fecha:12-04-2022<br> Materia: tallerbda Tarea: Mongo db");
+        
+      });
     
   });
